20cdfdedd30197807bc74ebeb434d5452a1e4a28
uemichi190 <uemichi190@gmail.com>
2026-04-04 05:46:37 +0900

    マージの失敗を修復

--- mymain.py
+++ mymain.py
@@ -7,14 +7,5 @@ def main():
     print(f'result of {fib_m.__name__}(10): {fib_m(10)}')
 
 if __name__ == '__main__':
-#
-    from mymath import fib, fib_l, fib_m
-
-def main():
-    print(f'result of {fib.__name__}(10): {fib(10)}')
-    print(f'result of {fib_l.__name__}(10): {fib_l(10)}')
-    print(f'result of {fib_m.__name__}(10): {fib_m(10)}')
-
-if __name__ == '__main__':
 # 5b35e2acb6c822b159da4f4255f6e59dca6d2b62
     main()
